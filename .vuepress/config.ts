ffdcd7397ae51735da455338e59c4e20c79582aa
creatliukun <952476356@qq.com>
2025-03-31 01:18:37 +0800

    feat: 加上时间轴

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -78,6 +78,7 @@ export default defineUserConfig({
       { text: '主页', link: '/', icon: 'IconHome' },
       { text: '分类', link: '/categories/reco/1.html', icon: 'IconDocument' },
       { text: '标签', link: '/tags/tag1/1.html', icon: 'IconTag' },
+      { text: '时间轴', link: '/timeline.html', icon: 'IconTime' },
       {
         text: '学习笔记',
         children: [
